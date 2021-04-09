08f934e72f86cc16c5af18c038d184f18936f06a
Michele Lisignoli <michele@lisignoli.com>
2021-04-09 21:05:37 +0200

    function search movies

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var app = new Vue ({
+   el: "#app",
+   data:{
+      movies: [],
+      search: ""
+   },
+   methods:{
+      getMovie: function(){
+         var self = this;
+         axios.get('https://api.themoviedb.org/3/search/movie', {
+            params: {
+               api_key: "f89e14052a0d0db382004cc67a28c7d5",
+               query: self.search,
+               language: "it-IT"
+            }
+         })
+         .then((serverAnswer) => {
+            this.movies = serverAnswer.data.results
+            this.search = ''
+         });  
+      }
+   }
+});
