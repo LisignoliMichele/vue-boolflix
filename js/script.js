57f80bf019ba6000646b13b9ea6396401c87906d
Michele Lisignoli <michele@lisignoli.com>
2021-04-12 17:40:03 +0200

    stars

--- js/script.js
+++ js/script.js
@@ -6,19 +6,29 @@ var app = new Vue ({
    },
    methods:{
       getMovie: function(){
-         var self = this;
          axios.get('https://api.themoviedb.org/3/search/movie', {
             params: {
                api_key: "f89e14052a0d0db382004cc67a28c7d5",
-               query: self.search,
+               query: this.search,
                language: "it-IT"
             }
          })
          .then((serverAnswer) => {
-            this.movies = serverAnswer.data.results
-            this.search = ''
-            console.log(this.movies)
-         });  
+            this.movies = [],
+            serverAnswer.data.results.forEach((movie) => { 
+
+               console.log(movie.vote_average)
+               console.log(movie.vote_average / 2)
+               console.log(Math.round(movie.vote_average / 2))
+
+               movie.vote_average = Math.round(movie.vote_average / 2);
+               this.movies.push(movie)
+            this.search = '';
+             });
+         });
       }
+      
    }
 });
+
+
