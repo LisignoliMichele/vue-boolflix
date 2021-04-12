d44c6e5d05316bc3cfc9a3a0723ce9319b214fe3
Michele Lisignoli <michele@lisignoli.com>
2021-04-12 18:00:17 +0200

    .HTML

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 var app = new Vue ({
    el: "#app",
    data:{
+      searchURL: "https://api.themoviedb.org/3/search/",
+      kind: ['movie', 'tv'],
+      kindIndex: 0,
       movies: [],
-      search: ""
+      search: "",
+      apiKey: "f89e14052a0d0db382004cc67a28c7d5",
+      languages: ["it-IT", "en-US", "es-ES"],
+      languagesIndex: 0,
    },
    methods:{
       getMovie: function(){
-         axios.get('https://api.themoviedb.org/3/search/movie', {
+         axios.get(this.searchURL + this.kind[this.kindIndex], {
             params: {
-               api_key: "f89e14052a0d0db382004cc67a28c7d5",
+               api_key: this.apiKey,
                query: this.search,
-               language: "it-IT"
+               language: this.languages[this.languagesIndex],
             }
          })
          .then((serverAnswer) => {
             this.movies = [],
             serverAnswer.data.results.forEach((movie) => { 
 
-               console.log(movie.vote_average)
-               console.log(movie.vote_average / 2)
-               console.log(Math.round(movie.vote_average / 2))
-
                movie.vote_average = Math.round(movie.vote_average / 2);
                this.movies.push(movie)
-            this.search = '';
+
+               this.search = '';
+
              });
          });
       }
