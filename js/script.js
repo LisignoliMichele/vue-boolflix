a677a2e1b29470538b68ee7ac5ccb0d7bde37e25
Michele Lisignoli <michele@lisignoli.com>
2021-04-12 19:15:50 +0200

    serie TV

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ var app = new Vue ({
       kind: ['movie', 'tv'],
       kindIndex: 0,
       movies: [],
+      series:[],
       search: "",
       apiKey: "f89e14052a0d0db382004cc67a28c7d5",
       languages: ["it-IT", "en-US", "es-ES"],
       languagesIndex: 0,
    },
    methods:{
+      
       getMovie: function(){
          axios.get(this.searchURL + this.kind[this.kindIndex], {
             params: {
@@ -25,14 +27,29 @@ var app = new Vue ({
 
                movie.vote_average = Math.round(movie.vote_average / 2);
                this.movies.push(movie)
-
-               this.search = '';
-
              });
          });
-      }
+
+         axios.get(this.searchURL + "tv", {
+            params: {
+               api_key: this.apiKey,
+               query: this.search,
+               language: this.languages[this.languagesIndex],
+            }
+         })
+         .then((serverAnswer) => {
+            this.series = [];
+            serverAnswer.data.results.forEach((serie) => { 
+         
+               serie.vote_average = Math.round(serie.vote_average / 2);
+               this.series.push(serie);
+         
+               this.search = '';
+             });
+         });
+
+      },
+         
       
    }
 });
-
-
